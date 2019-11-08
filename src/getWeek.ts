dbd3a166c172051aac6b4a014b13da7a40938770
Dante Monaldo <dante.monaldo@gmail.com>
2019-11-08 02:02:19 +0000

    Add full week test suite for UTC

--- src/getWeek.ts
+++ src/getWeek.ts
@@ -1,7 +1,6 @@
-export function getWeek(offset: number): number {
+import moment from 'moment';
+
+export function getWeek(date: Date, offset: number): number {
   let unixEpoch = new Date(0 - (offset || 0) * 60000);
-  let unixEpochMilliseconds = unixEpoch.getMilliseconds();
-  let now = new Date().getMilliseconds();
-  let numWeeks = Math.floor(((Math.round((now-unixEpochMilliseconds)/86400000))+(unixEpoch.getDay()+6)%7)/7);
-  return numWeeks;
+  return Math.floor(((Math.floor((date.getTime()-unixEpoch.getTime())/86400000))+(unixEpoch.getDay()+6)%7)/7);
 }
--- src/test/getWeek.test.ts
+++
@@ -1,5 +0,0 @@
-import { getWeek } from '../getWeek';
-
-test('init', () => {
-  expect(getWeek(0)).toBe(0);
-});
