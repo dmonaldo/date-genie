3cefd5e3be1e195028f84e24c1fcb71356f829cf
Dante Monaldo <dante.monaldo@gmail.com>
2019-11-14 16:21:40 -0800

    Accept no timezone offset for getWeek

--- src/getWeek.ts
+++ src/getWeek.ts
@@ -1,7 +1,5 @@
-import moment from 'moment';
-
-export function getWeek(date: Date, offset: number): number {
-  let unixEpoch = new Date(0 + (offset || 0) * 60000);
+export function getWeek(date: Date, offset: number = 0): number {
+  let unixEpoch = new Date(0 + offset * 60000);
   let numDays = Math.floor((date.getTime()-unixEpoch.getTime())/86400000);
   return Math.floor((numDays+(unixEpoch.getDay()+6)%7)/7);
 }
